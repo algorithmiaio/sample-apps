f51c5b2751c0fa0ebc17ef6ba1f725318129d48a
Anthony Nowell <anthony@algorithmia.com>
2016-04-27 14:19:00 -0600

    Remove build files

--- JavaScript/Analyze_readme/build/js/plugins-d701daac.js
+++
@@ -1,2 +0,0 @@
-// Avoid `console` errors in browsers that lack a console.
-(function(){var e,t=function(){},n=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeStamp","trace","warn"],r=n.length,i=window.console=window.console||{};while(r--)e=n[r],i[e]||(i[e]=t)})();

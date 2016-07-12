5d4420ca208896989819d56575fb589938de611d
Anthony Nowell <anthony@algorithmia.com>
2016-07-12 10:57:07 -0600

    Adding automation to publish multiple static sites

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+// This Gruntfile generates `publish` tasks,
+//   that upload a directory of static assets
+//   to a directory in the demos.algorithmia.com bucket
+//
+// Note: this doesn't currently do any asset path adjustments
+//  if the demo uses assets or links that start with '/',
+//  they probably won't load as expected
+
+module.exports = function(grunt) {
+
+  // These are the demos that we generate publish tasks for.
+  //    slug: generates a publish-<slug> task that publishes to demos.algorithmia.com/<slug>
+  //    dist: the directory containing a static files to publish (index.html should be in this directory)
+  var demos = [
+    { slug: 'colorize-photos', dist: 'JavaScript/colorization-demo/' },
+    { slug: 'classify-places', dist: 'JavaScript/places-demo/'},
+  ];
+
+
+  var awsS3Config = {
+      options: {
+        accessKeyId: '<%= aws.key %>',
+        secretAccessKey: '<%= aws.secret %>',
+        bucket: 'demos.algorithmia.com',
+        region: 'us-east-1',
+        uploadConcurrency: 5,
+        differential: true
+      }
+  };
+
+  demos.forEach(function(demo) {
+    awsS3Config[demo.slug] = {
+      files: [{
+        expand: true,
+        cwd: demo.dist,
+        src: ['**'],
+        dest: demo.slug
+      }]
+    };
+  });
+
+  grunt.initConfig({
+    aws: grunt.file.readJSON('aws-keys.json'),
+    aws_s3: awsS3Config
+  });
+  grunt.loadNpmTasks('grunt-aws-s3');
+
+  demos.forEach(function(demo) {
+    grunt.registerTask('publish:' + demo.slug, "Publish the " + demo.slug + " demo", ['aws_s3:' + demo.slug]);
+  });
+
+};
---
+++ PUBLISH.md
@@ -0,0 +1,43 @@
+## Publishing demos
+
+Most of the demos get published via Grunt tasks.
+
+```bash
+npm install -g grunt-cli
+npm install
+```
+
+You'll need to create `aws-keys.json` in the repo root:
+
+```json
+{
+  "key": <ACCESS_KEY_ID>,
+  "secret": <SECRET_ACCESS_KEY>
+}
+```
+
+Then you can publish a demo by slug name, for example:
+
+```bash
+grunt publish:colorize-photos
+```
+
+To list the available Grunt tasks:
+
+```
+grunt --help
+```
+
+## Adding new demo
+
+The grunt task just publishes a specific directory containing a static site to a directory in the demos.algorithmia.com bucket.
+
+To add a new static site, just add a small snippet to the `demos` array in `Gruntfile.js`,
+and a new grunt task will be available.
+
+```javascript
+var demos = [
+  { slug: 'colorize-photos', dist: 'JavaScript/colorization-demo/' },
+  { slug: 'classify-places', dist: 'JavaScript/places-demo/'},
+];
+```

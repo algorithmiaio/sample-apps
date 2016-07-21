b49d71e1cb3229ca843be797ebc956d5cb792946
James Athappilly <jamesatha@gmail.com>
2016-07-21 12:46:09 -0700

    Using argparse so you don't need to ever modify the code to get the demo to work

--- Python/colorize-directory/colorize.py
+++ Python/colorize-directory/colorize.py
@@ -1,33 +1,42 @@
 #!/usr/bin/python
-​
+
 import Algorithmia
-​
-#Set your Algorithmia API KEy
-apiKey = 'API KEY HERE'
-​
-#Initialize Algorithmia Python client
-client = Algorithmia.client(apiKey)
-​
-#Pick Algorithm to use
-algo = client.algo('deeplearning/ColorfulImageColorization/0.1.16')
-​
-#Iterate over regisrered dropbox + folder where my images are.
-for f in client.dir("dropbox://stein-Algorithmia/").list():
+import argparse
+import posixpath
 
-    #Check file if file type is supported.
-    if f.getName().lower().endswith(('.png','.jpg','.jpeg','.bmp','.gif')):
-        #Image progress write
-        print "Reading " + f.getName()
-​
-        #Define input for Algorithm + Parameters
-        input = {"image":"dropbox://stein-Algorithmia/" + f.getName(),"location":"dropbox://stein-Algorithmia/color_" + f.getName()}
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--api-key', required=True)
+    parser.add_argument('--connector-path', default='dropbox://') # uses the default dropbox connector is none is specified
+    args = parser.parse_args()
 
-        #Call Algorithm
-        output = algo.pipe(input)
-        print "Colorizing: color_" + f.getName()
+    # Initialize Algorithmia Python client
+    client = Algorithmia.client(args.api_key)
 
-        #client.file('dropbox://Nathan/thumbnail_' + f.getName()).put(output.result)
-    else:
-        print "File:" + f.getName() +  "is not a type that is supported."
-​
-print "Done processing..."
+    # Pick Algorithm to use
+    algo = client.algo('deeplearning/ColorfulImageColorization/0.1.16')
+
+    top_level_dir = client.dir(args.connector_path)
+
+    # Iterate over regisrered dropbox + folder where my images are.
+    for f in top_level_dir.files():
+
+        # Check file if file type is supported.
+        if f.getName().lower().endswith(('.png','.jpg','.jpeg','.bmp','.gif')):
+            colored_file_name = 'color_' + f.getName()
+
+            # Define input for Algorithm + Parameters
+            algo_input = { 'image':    posixpath.join(args.connector_path, f.getName()),
+                           'location': posixpath.join(args.connector_path, colored_file_name)}
+
+            print 'Processing {} and saving to {}'.format(f.getName(), colored_file_name)
+
+            # Call Algorithm
+            output = algo.pipe(algo_input)
+        else:
+            print "File: " + f.getName() +  " is not a type that is supported."
+
+    print "Done processing..."
+
+if __name__ == '__main__':
+    main()

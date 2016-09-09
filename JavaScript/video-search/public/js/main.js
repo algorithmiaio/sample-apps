7318d8927c74818e558a20e2446cff84afb3c140
zeryx <james@algorithmia.com>
2016-09-08 19:40:55 -0700

    filtered out "hot dog" temporarily so the search "dog" works.

--- JavaScript/video-search/public/js/main.js
+++ JavaScript/video-search/public/js/main.js
@@ -36,7 +36,7 @@ function search(query) {
     "minConfidence": 0.10
   };
   Algorithmia.client(Algorithmia.api_key)
-    .algo("algo://algorithmiahq/VideoClassificationDemo/0.5.4")
+    .algo("algo://algorithmiahq/VideoClassificationDemo/0.5.5")
     .pipe(algoInput)
     .then(function(output) {
       if(output.error) {

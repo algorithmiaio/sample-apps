aad107f3101fea5f6d8174759a1702a844863e96
zeryx <james@algorithmia.com>
2016-09-09 08:22:44 -0700

    uprevved to json file download caching, looks like this halved algo execution time.

--- JavaScript/video-search/public/js/main.js
+++ JavaScript/video-search/public/js/main.js
@@ -36,7 +36,7 @@ function search(query) {
     "minConfidence": 0.15
   };
   Algorithmia.client(Algorithmia.api_key)
-    .algo("algo://algorithmiahq/VideoClassificationDemo/0.5.5")
+    .algo("algo://algorithmiahq/VideoClassificationDemo/0.5.6")
     .pipe(algoInput)
     .then(function(output) {
       if(output.error) {

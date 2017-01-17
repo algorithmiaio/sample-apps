2851712f4e60d97ebb35fb1755960b3d6837f182
Jon Peck <peckjon@gmail.com>
2017-01-17 12:23:58 -0800

    rss-dashboard demo: clean up DOM IDs; allow Enter button to trigger demo; autoscroll to results section

--- JavaScript/RSS_dashboard/public/js/main.js
+++ JavaScript/RSS_dashboard/public/js/main.js
@@ -2,22 +2,19 @@
 var algoClient = Algorithmia.client('simeyUbLXQ/R8Qga/3ZCRGcr2oR1');
 
 var httpRegex = new RegExp('^(http|https)://', 'i');
-var statusLabel, output;
 
 /**
  * once DOM is ready, update vars amd set initial URL
  */
 $(document).ready(function() {
-  statusLabel = $('#status-label');
-  output = $('#output');
-  updateUrl();
+  updateRssUrl();
 });
 
 /**
  * copy URL of RSS feed from dropdown into input text
  */
-function updateUrl() {
-  $('#inputStrings').val($('#urlDD :selected').val());
+function updateRssUrl() {
+  $('#rssUrl').val($('#rssSelector :selected').val());
 }
 
 /**
@@ -63,16 +60,20 @@ function sentimentScoreToText(sentimentScore) {
  * scrape the RSS feed and analyze its entries
  */
 var processFeed = function() {
-  // clear out old results
-  output.empty();
-  statusLabel.empty();
-  $('#algo-spinner').removeClass('hidden');
+  // clear out any old results
+  $('#results').empty();
+  $('#status-label').empty();
+  $('#fetch-spinner').removeClass('hidden');
   // query RSS scraper algorithm with selected feed URL
-  var feedUrl = prefixHttp($('#inputStrings').val());
+  var feedUrl = prefixHttp($('#rssUrl').val());
   algoClient.algo('/tags/ScrapeRSS').pipe(feedUrl).then(function(response) {
-    $('#algo-spinner').addClass('hidden');
+    $('#fetch-spinner').addClass('hidden');
     if (displayError(response, 'Failed to load RSS feed; please check that it is a valid URL')) {return;}
-    $('#results').removeClass('hidden');
+    $('#resultsWrapper').removeClass('hidden');
+    // smooth scroll to results section
+    $('html, body').animate({
+      scrollTop: $("#resultsWrapper").offset().top
+    }, 1000);
     // process no more than 6 RSS entries
     response.result.slice(0,6).forEach(processFeedEntry);
   });
@@ -81,15 +82,14 @@ var processFeed = function() {
 
 /**
  * insert a section into page showing an RSS feed entry's title, generated summary & tags, and sentiment analysis 
- * @param url web address to which the entry points 
- * @param title entry's title from RSS feed
+ * @param entry {{url:string,title:string}} feed entry with URL and Title
  */
-var processFeedEntry = function(url, title) {
+var processFeedEntry = function(entry) {
   // create section and elements into which we will stick the results of our algorithms
   var row = $('<section><div class="row whitespace-none rss-result">'
     + '<div class="col-md-12 col-lg-9">'
     + '<p class="item-title">ARTICLE TITLE</p>'
-    + '<h4 class="result"><a href="' + url + '">' + title + '</a></h4>'
+    + '<h4 class="result"><a href="' + entry.url + '">' + entry.title + '</a></h4>'
     + '<p class="item-title">GENERATED SUMMARY</p>'
     + '<p class="summary"><span class="aspinner"></span></p>'
     + '<p class="item-title">GENERATED TAGS</p>'
@@ -97,10 +97,10 @@ var processFeedEntry = function(url, title) {
     + '<p class="item-title">SENTIMENT ANALYSIS</p>'
     + '<div class="sentiment"><span class="aspinner"></span></div>'
     + '</div></section>');
-  output.append(row);
+  $('#results').append(row);
   // fetch text from the page to which this entry points, and extract summary, tags, and sentiment
-  algoClient.algo('/util/Html2Text').pipe(url).then(function (response) {
-    if (displayError(response, 'Error fetching ' + url + ': ' + response.error)) {
+  algoClient.algo('/util/Html2Text').pipe(entry.url).then(function (response) {
+    if (displayError(response, 'Error fetching ' + entry.url + ': ' + response.error)) {
       return;
     }
     summarizeFeedEntry(response.result, row.find('.summary:first'));

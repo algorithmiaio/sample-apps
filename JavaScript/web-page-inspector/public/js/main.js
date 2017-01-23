48a57a7b3074e6dd735fc9e08f5d4efd794953a7
Jon Peck <peckjon@gmail.com>
2017-01-23 11:00:24 -0800

    web-page-inspector: document and clean up functions

--- JavaScript/web-page-inspector/public/js/main.js
+++ JavaScript/web-page-inspector/public/js/main.js
@@ -1,6 +1,6 @@
-window.Algorithmia = window.Algorithmia || {};
-Algorithmia.api_key = "simnxB3dwTN8kds9p6SGMpGoOJC1"
-var numTasks = 0;
+// init the Algorithmia client with your API key from https://algorithmia.com/user#credentials
+var algoClient = Algorithmia.client('simnxB3dwTN8kds9p6SGMpGoOJC1');
+var algorithmAutotag = 'outofstep/MegaAnalyzeURL/0.1.6';
 
 /**
  * once DOM is ready, update vars amd set initial URL
@@ -9,53 +9,45 @@ $(document).ready(function() {
   setInviteCode('analyzeurl');
 });
 
-var callAlgorithm = function() {
-  // begin tasks
-  startTask();
-  // Get the img URL
-  var url = $("#url").val();
-  // Remove any whitespace from url
+/**
+ * analyze the url specified in $('#url') by calling outofstep/MegaAnalyzeURL
+ * @param url (optional) url to use instead of $('#url') contents
+ */
+var analyzeUrl = function(url) {
+  if(url) {
+	  $("#url").val(url);
+  } else {
+    url = $("#url").val();
+  }
   url = url.trim();
-
-  // Validate the URL
-  ValidURL(url);
-};
-
-var ValidURL = function(url) {
-  if(!isValidUrl(url)) {
+  showWait();
+  if(isValidUrl(url)) {
+    // call analysis algorithm with selected URL
+    algoClient.algo(algorithmAutotag).pipe(url).then(function(output) {
+      if (output.error) {
+        console.log("There was an error", output.error.message);
+        $("#status-label").html('<div class="alert alert-danger" role="alert">' + output.error.message + ' </div>');
+        endWait();
+      } else {
+        // Add results to page
+        showResults(output.result);
+      }
+     });
+  } else {
     // Error Handling
     $("#status-label").html('<div class="alert alert-danger" role="alert">Please enter a valid URL.</div>');
-    resetView();
-  } else {
-    console.log("URL is valid");
-    // Call algorithm
-    getMeta(url);
+    endWait();
   }
 };
 
-var getMeta = function(url){
-  console.log("calling algorithm")
-  Algorithmia.client(Algorithmia.api_key)
-   .algo("algo://outofstep/MegaAnalyzeURL/0.1.6")
-   .pipe(url)
-   .then(function(output) {
-      if (output.error) {
-        console.log("There was an error", output.error.message);
-        // Error Handling
-        var statusLabel = $("#status-label");
-        statusLabel.html('<div class="alert alert-danger" role="alert">' + output.error.message + ' </div>');
-        resetView();
-      } else {
-        // Add results to page
-        addMeta(output.result);
-      }
-   });
-};
+var showResults = function(data){
 
-var addMeta = function(data){
-  console.log("Adding Metadata");
-  $("#code").text(JSON.stringify(data, null, 4));
-  hljs.highlightBlock($("#code")[0]);
+  // Add raw JSON response to results section
+  var codeblock = $("#code");
+  codeblock.text(JSON.stringify(data, null, 4));
+  codeblock.each(function(i,block) {hljs.highlightBlock(block);});
+
+  // Add simple result values
   $("#title").text(data.metadata.title);
   $("#thumb").attr('src', data.metadata.thumbnail);
   $("#shortsummary").text(data.metadata.summary);
@@ -66,8 +58,8 @@ var addMeta = function(data){
 
   // Add URL
   var url = $("#userurl");
+  url.text(data.metadata.url);
   url.attr('href', data.metadata.url);
-  url.attr(data.metadata.url);
 
   // Add Sentiment Analysis
   if (data.socialsentiment){
@@ -91,9 +83,7 @@ var addMeta = function(data){
     var newSpan = document.createElement('span');
     newSpan.className = "label label-primary";
     newSpan.innerHTML = i;
-
     var space = document.createTextNode(" ");
-
     tags.append(newSpan);
     tags.append(space);
   };
@@ -105,32 +95,27 @@ var addMeta = function(data){
     var listLink = document.createElement('a');
     listLink.setAttribute('href', data.links[i]);
     listLink.innerHTML = data.links[i];
-
     links.append(listItem);
     listItem.append(listLink);
-  };
-
-  var createSocialSection = function(title, body) {
-    $('<span class="label label-default">'+title+'<span class="badge">'+body+'</span></span>')
   };
 
   // Add social shares
   var shares = $("#socialshares");
   console.log(data.socialshares);
   if (data.socialshares.facebook_likes > 0) {
-    createSocialSection("Facebook Likes: ", data.socialshares.facebook_likes);
+    shares.append(createSocialSection("Facebook Likes: ", data.socialshares.facebook_likes));
   }
   if (data.socialshares.facebook_shares > 0) {
-    createSocialSection("Facebook Shares: ", data.socialshares.facebook_shares);
+    shares.append(createSocialSection("Facebook Shares: ", data.socialshares.facebook_shares));
   }
   if (data.socialshares.facebook_comments > 0) {
-    createSocialSection("Facebook Comments: ", data.socialshares.facebook_comments);
+    shares.append(createSocialSection("Facebook Comments: ", data.socialshares.facebook_comments));
   }
   if (data.socialshares.linkedIn > 0) {
-    createSocialSection("LinkedIn Shares: ", data.socialshares.linkedIn);
+    shares.append(createSocialSection("LinkedIn Shares: ", data.socialshares.linkedIn));
   }
   if (data.socialshares.pinterest > 0) {
-    createSocialSection("Pinterest Shares: ", data.socialshares.pinterest);
+    shares.append(createSocialSection("Pinterest Shares: ", data.socialshares.pinterest));
   }
 
   // Add images
@@ -140,35 +125,42 @@ var addMeta = function(data){
     if (data.images[i].match(/\.(jpeg|jpg|gif|png|svg)$/) != null) {
       var div = document.createElement('div');
       div.className = "col-sm-4";
-
       var img = document.createElement('img');
       img.setAttribute('src', data.images[i]);
       img.className = "img-responsive";
-
       imgs.append(div);
       div.append(img);
     }
-  };
+  }
 
-  console.log(data);
-  // Finish adding to DOM
-  finishTask();
+  // reveal and smooth-scroll to results section
+  endWait(true);
+  $('html, body').animate({
+    scrollTop: $("#resultspage").offset().top
+  }, 1000);
+
 };
 
-var analyzeDefault = function(url) {
-	$("#url").val(url);
-	callAlgorithm();
+/**
+ * Create a social-shares section to display
+ * @param title
+ * @param body
+ * @returns {jQuery|HTMLElement}
+ */
+var createSocialSection = function(title, body) {
+  return $('<span class="label label-default">'+title+'<span class="badge">'+body+'</span></span>')
 };
 
-var startTask = function() {
-  numTasks++;
+/**
+ * show overlay/dots and hide button; pre-clear results
+ */
+var showWait = function() {
   $("#overlay").removeClass("hidden");
   $(".dots-container").removeClass("hidden");
   $("#analyze-button").addClass("hidden");
-
+  $("#results").addClass("hidden");
   // Clear error messages
   $("#status-label").empty();
-
   // Clear contents
   $("#title").empty();
   $("#thumb").attr('src','');
@@ -187,23 +179,14 @@ var startTask = function() {
   $("#negative").empty();
 };
 
-var finishTask = function() {
-  numTasks--;
-  if(numTasks <= 0) {
-    resetView(true);
-    // smooth scroll to results section
-    $('html, body').animate({
-      scrollTop: $("#resultspage").offset().top
-    }, 1000);
-  }
-};
-
-var resetView = function(showResults) {
-  numTasks = 0;
+/**
+ * hide overlay & dots, reveal button
+ * @param showResults should the results section be revealed?
+ */
+var endWait = function(showResults) {
   $("#overlay").addClass("hidden");
   $(".dots-container").addClass("hidden");
   $("#analyze-button").removeClass("hidden");
-  $("#fetch-spinner").addClass("hidden");
   if(showResults) {
     $("#results").removeClass("hidden");
   } else {

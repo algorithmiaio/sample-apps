82677d4e25d3f45f92e1eae8fcf5bfa34d78b14b
Jon Peck <peckjon@gmail.com>
2017-01-24 12:28:54 -0800

    language detector recipe: sort results and return the language with the highest confidence

--- recipes/language-detector/language-detector.py
+++ recipes/language-detector/language-detector.py
@@ -8,7 +8,9 @@ client = Algorithmia.client("your_api_key")
 def detect_language(text):
     """detect the language of a piece of text and return the ISO 639 language code"""
     algo = client.algo('nlp/LanguageIdentification/1.0.0')
-    return algo.pipe({'sentence':text}).result[0]['language']
+    result = algo.pipe({'sentence':text}).result
+    result_sorted = sorted(result, key=lambda r: r['confidence'], reverse=True)
+    return result_sorted[0]['language']
 
 def extract_text(filename):
     """Extract and return the text from a document"""

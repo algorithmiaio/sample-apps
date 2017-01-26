f6c4c3b9ba1e298b43bc0ae714397239a064abdf
Jon Peck <peckjon@gmail.com>
2017-01-25 16:52:28 -0800

    video-search: complete rewrite: jQuery conversion, documentation, new output format

--- JavaScript/video-search/public/js/main.js
+++ JavaScript/video-search/public/js/main.js
@@ -1,195 +1,144 @@
-// clouds https://images.unsplash.com/16/unsplash_525a7e89953d1_1.JPG
-// hills https://images.unsplash.com/29/cloudy-hills.jpg
-// rock https://images.unsplash.com/reserve/yZfr4jmxQyuaE132MWZm_stagnes.jpg
-// paris https://images.unsplash.com/33/YOfYx7zhTvYBGYs6g83s_IMG_8643.jpg
+// init the Algorithmia client with your API key from https://algorithmia.com/user#credentials
+var algoClient = Algorithmia.client('sim4m8jnVOF3086ujBXdiYteIS01');
+var algorithmVideoClassification = "algorithmiahq/VideoClassificationDemo/0.5.9";
+window.player = null;
 
 /**
  * once DOM is ready, update vars amd set initial URL
  */
 $(document).ready(function() {
-  setInviteCode('color');
+  setInviteCode('videosearch');
 });
 
-window.Algorithmia = window.Algorithmia || {};
-Algorithmia.api_key = "sim4m8jnVOF3086ujBXdiYteIS01";
-var numTasks = 0;
-
-function callAlgorithm() {
-  var statusLabel = document.getElementById("status-label");
-  statusLabel.innerHTML = "";
-
-  // Get the search query URL
-  var query = document.getElementById("search-query").value.trim();
-
-  if(typeof(query) == "string" && query !== "") {
-    startTask();
-    search(query);
+/**
+ * run the specified query against algorithmiahq/VideoClassificationDemo and render the results
+ * @param query (optional) query to use instead of $('#search-query') contents
+ */
+var search = function(query) {
+  $("#status-label").empty();
+  if(query) {
+    $("#search-query").val(query);
+  } else {
+    query = $("#search-query").val().trim();
   }
-
-};
-
-function search(query) {
-  console.log("Searching for tag", query);
-
-  // renderSearchResults([
-  //   {title: "Deadmau5 - Ghosts N Stuff", videoId: "3Gb3faOzvBk", firstFrame: 10, lastFrame: 100},
-  //   {title: "Sean Mackey - Discover", videoId: "Ts2I4ffd4p8", firstFrame: 20, lastFrame: 200}
-  // ]);
-  // finishTask();
-
+  if(!query) {
+    return;
+  }
+  showWait();
   var algoInput = {
     "collections": ["data://demo/VideoSearchDemo"],
     "keyword": query,
     "minConfidence": 0.15
   };
-  Algorithmia.client(Algorithmia.api_key)
-    .algo("algo://algorithmiahq/VideoClassificationDemo/0.5.9")
-    .pipe(algoInput)
-    .then(function(output) {
-      if(output.error) {
-        // Error Handling
-        var statusLabel = document.getElementById("status-label")
-        statusLabel.innerHTML = '<div class="alert alert-danger" role="alert">Uh Oh! Something went wrong: ' + output.error.message + ' </div>';
-        taskError();
-      } else {
-        console.log("got output", output.result);
-
-        // Render search results
-        try {
-          renderSearchResults(output.result);
-        } catch(e) {
-          console.log("error rendering", e);
-        }
-        finishTask();
+  algoClient.algo(algorithmVideoClassification).pipe(algoInput).then(function(output) {
+    if(output.error) {
+      // Error Handling
+      showError(output.error.message);
+    } else if (output.result.length==0) {
+      showError('We couldn\'t find any videos for "'+query+'"');
+    } else {
+      // Render search results
+      try {
+        renderSearchResults(query, output.result);
+        endWait(true);
+      } catch(e) {
+        console.log("error rendering", e);
+        showError(e.message)
       }
-    });
-}
-
-function renderSearchResults(results) {
-  console.log("renderSearchResults", results);
-  var output = document.getElementById("search-results");
-  output.innerHTML = "";
-  for(var i = 0; i < results.length; i++) {
-    var doc = results[i];
-    console.log("renderSearchResults doc", doc);
-    var li = document.createElement("li");
-
-    var thumb = document.createElement("img");
-    thumb.src = "http://img.youtube.com/vi/" + doc.videoId + "/0.jpg"
-    thumb.classList.add("thumb")
-
-    var info = document.createElement("div");
-    info.classList.add("video-info");
-
-    // Main youtube link
-    var link = document.createElement("a");
-    link.innerText = doc.title;
-    link.onclick = jumpToVideo(doc, 0);
-    var linkDiv = document.createElement("div");
-    linkDiv.classList.add("video-title");
-    linkDiv.appendChild(link);
-    info.appendChild(linkDiv);
-
-    // Stats and metadata
-    var linkFirst = document.createElement("a");
-    linkFirst.innerText = formatTime(doc.startFrame);
-    linkFirst.onclick = jumpToVideo(doc, doc.startFrame);
-    var linkLast = document.createElement("a");
-    linkLast.innerText = formatTime(doc.stopFrame);
-    linkLast.onclick = jumpToVideo(doc, doc.stopFrame);
-
-    info.appendChild(document.createTextNode("Appearance: "));
-    info.appendChild(linkFirst);
-    info.appendChild(document.createTextNode(" - "));
-    info.appendChild(linkLast);
-    // info.appendChild(document.createTextNode(")"));
-
-    // Score
-    var scoreDiv = document.createElement("div");
-    scoreDiv.appendChild(document.createTextNode("Score: " + doc.avgConfidence.toFixed(3)));
-    info.appendChild(scoreDiv);
-
-    li.appendChild(thumb);
-    li.appendChild(info);
-    output.appendChild(li);
-  }
-}
-
-function formatTime(seconds) {
-  var mod60 = Math.floor(seconds % 60);
-  if (mod60 < 10) {
-    mod60 = "0" + mod60;
-  }
-  return Math.floor(seconds / 60) + ":" + mod60;
-}
-
-function jumpToVideo(doc, time) {
-  return function() {
-    console.log("Jumping to " + doc.title + " @ " + time);
-    player.loadVideoById(doc.videoId);
-    player.seekTo(time);
-    player.playVideo();
-  };
-}
-
-function startTask() {
-  numTasks++;
-  document.getElementById("overlay").classList.remove("hidden");
-}
-
-function finishTask() {
-  numTasks--;
-  if(numTasks <= 0) {
-    document.getElementById("overlay").classList.add("hidden");
-    document.getElementById("explainer").classList.add("hidden");
-    document.getElementById("results").classList.remove("hidden");
-  }
-}
-
-function taskError() {
-  numTasks = 0;
-  document.getElementById("overlay").classList.add("hidden");
-  document.getElementById("explainer").classList.add("display");
-  document.getElementById("explainer").classList.remove("hidden");
-  document.getElementById("results").classList.add("hidden");
-}
-
-document.getElementById("search-form").onsubmit = function(e) {
-  console.log("onsubmit");
-  e.preventDefault();
-  callAlgorithm();
-  return false;
-}
-
-
-window.player = null;
-
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-function onYouTubeIframeAPIReady() {
-  console.log("YT ready");
-  player = new YT.Player('player', {
-    // height: '390',
-    // width: '640',
-    // videoId: '3Gb3faOzvBk',
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
     }
   });
-}
+};
 
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  console.log("player ready");
-  // event.target.playVideo();
-}
+/**
+ * display results of the query inside $('#search-results')
+ * @param query
+ * @param results
+ */
+var renderSearchResults = function(query, results) {
+  $('#search-results-query').text('"'+query+'"');
+  var output = $('#search-results');
+  output.empty();
+  results.forEach(function(doc) {
+    output.append('<li class="row video-result">'
+      +'  <div class="col-sm-6"><div class="video-container">'+getIframeHtml(doc)+'</div></div>'
+      +'  <div class="col-sm-6"><ul class="video-details">'
+      +'    <li><p class="item-title">TERM</p><p class="lg">'+query+'</p></li>'
+      +'    <li><p class="item-title">APPEARANCE</p><h4>'+getLinksHtml(doc)+'</h4></li>'
+      +'    <li><p class="item-title">VIDEO</p><p class="lg">'+doc.title+'</p></li>'
+      +'    <li><p class="item-title">CONFIDENCE</p><p class="lg">'+doc.avgConfidence.toFixed(3)+'</p></li>'
+      +'  </ul></div>'
+      +'</li>');
+  });
+};
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-function onPlayerStateChange(event) {
-  // if (event.data == YT.PlayerState.PLAYING &&) {
-  //   setTimeout(stopVideo, 6000);
-  // }
-}
+/**
+ * get YouTube iframe embed HTML
+ * @param doc
+ * @returns {string}
+ */
+var getIframeHtml = function(doc) {
+  return '<iframe id="video_'+doc.videoId+'" width="560" height="315" src="https://www.youtube.com/embed/'+doc.videoId+'" frameborder="0" allowfullscreen></iframe>';
+};
+
+/**
+ * get links HTML to jump video to start and end frames
+ * @param doc
+ * @returns {string}
+ */
+var getLinksHtml = function(doc) {
+  return '<a onclick="jumpToVideo(\''+doc.videoId+'\','+doc.startFrame+')">'+formatTime(doc.startFrame)+'</a>'
+    +' - <a onclick="jumpToVideo(\''+doc.videoId+'\','+doc.stopFrame+')">'+formatTime(doc.stopFrame)+'</a>';
+};
+
+/**
+ * format a number of seconds as M:SS
+ * @param seconds
+ * @returns {string}
+ */
+var formatTime = function(seconds) {
+  return Math.floor(seconds / 60)+":"+padleft(Math.floor(seconds % 60),2);
+};
+
+/**
+ * jump the specified video iframe to a specific timepoint
+ * @param videoId
+ * @param time
+ */
+var jumpToVideo = function(videoId, time) {
+  $('#video_'+videoId).attr('src','https://www.youtube.com/embed/'+videoId+'?start='+time+'&autoplay=1');
+};
+
+/**
+ * show dots, hide button, disable links; pre-clear results
+ */
+var showWait = function() {
+  $("#analyze-button-text").addClass("no-viz");
+  $('#search-query').attr('disabled','disabled');
+  $('.search-link').click(function (e) {e.preventDefault();});
+  $(".dots-container").removeClass("hidden");
+};
+
+/**
+ * hide dots, reveal button, enable links
+ * @param showResults should the results section be revealed?
+ */
+var endWait = function(showResults) {
+  $("#analyze-button-text").removeClass("no-viz");
+  $('#search-query').removeAttr('disabled');
+  $('.search-link').unbind('click');
+  $(".dots-container").addClass("hidden");
+  if(showResults) {
+    $("#results").removeClass("hidden");
+    $('html, body').animate({
+      scrollTop: $("#results").offset().top
+    }, 1000);
+  } else {
+    $("#results").addClass("hidden");
+    $('#search-results').empty();
+  }
+};
+
+var showError = function(message) {
+  $("#status-label").html('<div class="alert alert-danger" role="alert">'+message+' </div>');
+  endWait(false);
+};

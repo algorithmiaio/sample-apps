a4e91943d69eed0f79ee3e799c3c83f99e764837
Jon Peck <peckjon@gmail.com>
2017-01-30 11:00:42 -0800

    timeseries: de-Angularizing code

--- JavaScript/time-series/public/js/main.js
+++ JavaScript/time-series/public/js/main.js
@@ -7,6 +7,7 @@ var algorithmBuildingPermits = 'ETL/GetBuildingPermitData/0.1.5';
  */
 $(document).ready(function() {
   setInviteCode('timeseries');
+  analyze();
 });
 
 
@@ -14,8 +15,6 @@ $(document).ready(function() {
  * TBD: decide whether to stay on Angular1 or port to jQuery for simplicity
  */
 var app, cities, copy, defaultOptions, defaultSeries, transpose, updateViz;
-
-app = angular.module("algorithmia");
 
 defaultOptions = {
   width: $("#tsControl").width(),
@@ -104,144 +103,126 @@ cities = {
   }
 };
 
-app.controller("TimeSeriesControl", function($scope, $http) {
-  $scope.headers = ['Time', 'Value'];
-  $scope.tsOptions = defaultOptions;
-  $scope.tsData = defaultSeries;
-  $scope.dataSource = "Edmonton";
-  $scope.dataFilter1 = "/util/Echo";
-  $scope.dataFilter2 = "/util/Echo";
-  $scope.dataAnalysis = "/timeseries/Forecast";
-  $scope.analyze = function() {
-    if (!$scope.dataSource) {
-      return;
-    }
-    console.log("Fetching data...", $scope.dataSource);
-    algoClient.algo(algorithmBuildingPermits).pipe($scope.dataSource).then(function(result1) {
-      var timeseries, timestamps;
-      if (!result1.error) {
-        timestamps = result1.result[0].map(function(ts) {
-          return new Date(ts.replace(" ", "T"));
-        });
-        timeseries = result1.result[1];
-        updateViz(timestamps, timeseries, [], []);
-        console.log("Filtering1...", $scope.dataFilter1);
-        return algoClient.algo($scope.dataFilter1).pipe(timeseries).then(function(result2) {
-          var timeseriesFiltered1;
-          if (!result2.error) {
-            timeseriesFiltered1 = result2.result;
-            updateViz(timestamps, timeseries, timeseriesFiltered1, []);
-            console.log("Filtering2...", $scope.dataFilter2);
-            return algoClient.algo($scope.dataFilter2).pipe(timeseriesFiltered1).then(function(result3) {
-              var timeseriesFiltered2;
-              if (!result3.error) {
-                timeseriesFiltered2 = result3.result;
-                updateViz(timestamps, timeseries, timeseriesFiltered2, []);
-                console.log("Analyzing...", $scope.dataAnalysis);
-                return algoClient.algo($scope.dataAnalysis).pipe(timeseriesFiltered2).then(function(result4) {
-                  var dataSize, forecastSize, i, lastDate, timeseriesAnalysis, _i, _ref;
-                  if (!result4.error) {
-                    timeseriesAnalysis = result4.result;
-                    if ($scope.dataAnalysis === "/timeseries/Forecast") {
-                      dataSize = timestamps.length;
-                      forecastSize = timeseriesAnalysis.length;
-                      timeseriesAnalysis = [];
-                      lastDate = timestamps[timestamps.length - 1];
-                      for (i = _i = 0, _ref = dataSize + forecastSize - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
-                        if (i < dataSize) {
-                          timeseriesAnalysis[i] = null;
-                        } else {
-                          timeseriesAnalysis[i] = result4.result[i - dataSize];
-                          timestamps[i] = new Date(lastDate);
-                          timestamps[i].setMonth(lastDate.getMonth() + i - dataSize + 1);
-                        }
+var headers = ['Time', 'Value'];
+var tsOptions = defaultOptions;
+var tsData = defaultSeries;
+var dataSource = "Edmonton";
+var dataFilter1 = "/util/Echo";
+var dataFilter2 = "/util/Echo";
+var dataAnalysis = "/timeseries/Forecast";
+var analyze = function() {
+  $('#errorMessage').empty();
+  console.log("Fetching data...", $('#dataSource').val());
+  algoClient.algo(algorithmBuildingPermits).pipe($('#dataSource').val()).then(function(result1) {
+    var timeseries, timestamps;
+    if (!result1.error) {
+      timestamps = result1.result[0].map(function(ts) {
+        return new Date(ts.replace(" ", "T"));
+      });
+      timeseries = result1.result[1];
+      updateViz(timestamps, timeseries, [], []);
+      console.log("Filtering1...", $('#dataFilter1').val());
+      return algoClient.algo($('#dataFilter1').val()).pipe(timeseries).then(function(result2) {
+        var timeseriesFiltered1;
+        if (!result2.error) {
+          timeseriesFiltered1 = result2.result;
+          updateViz(timestamps, timeseries, timeseriesFiltered1, []);
+          console.log("Filtering2...", $('#dataFilter2').val());
+          return algoClient.algo($('#dataFilter1').val()).pipe(timeseriesFiltered1).then(function(result3) {
+            var timeseriesFiltered2;
+            if (!result3.error) {
+              timeseriesFiltered2 = result3.result;
+              updateViz(timestamps, timeseries, timeseriesFiltered2, []);
+              var dataAnalysisAlgo = $('#dataAnalysis');
+              console.log("Analyzing...", dataAnalysisAlgo);
+              return algoClient.algo(dataAnalysisAlgo).pipe(timeseriesFiltered2).then(function(result4) {
+                var dataSize, forecastSize, i, lastDate, timeseriesAnalysis, _i, _ref;
+                if (!result4.error) {
+                  timeseriesAnalysis = result4.result;
+                  if (dataAnalysisAlgo === "/timeseries/Forecast") {
+                    dataSize = timestamps.length;
+                    forecastSize = timeseriesAnalysis.length;
+                    timeseriesAnalysis = [];
+                    lastDate = timestamps[timestamps.length - 1];
+                    for (i = _i = 0, _ref = dataSize + forecastSize - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+                      if (i < dataSize) {
+                        timeseriesAnalysis[i] = null;
+                      } else {
+                        timeseriesAnalysis[i] = result4.result[i - dataSize];
+                        timestamps[i] = new Date(lastDate);
+                        timestamps[i].setMonth(lastDate.getMonth() + i - dataSize + 1);
                       }
                     }
-                    $scope.$apply(function() {
-                      if ($scope.dataAnalysis === "/timeseries/AutoCorrelate") {
-                        $scope.tsOptions = copy(defaultOptions);
-                        $scope.tsOptions.series[2].targetAxisIndex = 1;
-                        return $scope.tsOptions.vAxes[1] = {
-                          textPosition: 'none'
-                        };
-                      } else {
-                        return $scope.tsOptions = defaultOptions;
-                      }
-                    });
-                    updateViz(timestamps, timeseries, timeseriesFiltered2, timeseriesAnalysis);
-                    return console.log("Analysis", timeseriesAnalysis);
-                  } else {
-                    console.error("Failed to analyze data", result4.error);
-                    return $scope.errorMessage = "Failed to analyze data";
                   }
-                });
-              } else {
-                console.error("Failed to filter2 data", result3.error);
-                return $scope.errorMessage = "Failed to filter2 data";
-              }
-            });
-          } else {
-            console.error("Failed to filter1 data", result2.error);
-            return $scope.errorMessage = "Failed to filter1 data";
-          }
-        });
-      } else {
-        console.error("Failed to load data", result1.error);
-        return $scope.errorMessage = "Failed to load data";
-      }
-    });
-  };
-});
-
-app.directive("timeseriesChart", function() {
-  return {
-    restrict: 'E',
-    scope: {
-      headers: '=',
-      data: '=',
-      options: '='
-    },
-    link: function(lscope, element, attrs, controller) {
-      var chart, redraw;
-      chart = new google.visualization.ScatterChart(element[0]);
-      redraw = function(scope, element) {
-        var chartdata, headerWrapper, tsData;
-        if (scope.headers && scope.data && scope.data.length > 0) {
-          headerWrapper = [scope.headers];
-          tsData = (function() {
-            if (typeof scope.data === "string") {
-              try {
-                return JSON.parse(scope.data);
-              } catch (_error) {
-                return defaultSeries;
-              }
+                  if (dataAnalysisAlgo === "/timeseries/AutoCorrelate") {
+                    tsOptions = copy(defaultOptions);
+                    tsOptions.series[2].targetAxisIndex = 1;
+                    tsOptions.vAxes[1] = {textPosition: 'none'};
+                  } else {
+                    tsOptions = defaultOptions;
+                  }
+                  updateViz(timestamps, timeseries, timeseriesFiltered2, timeseriesAnalysis);
+                  return console.log("Analysis", timeseriesAnalysis);
+                } else {
+                  console.error("Failed to analyze data", result4.error);
+                  return $('#errorMessage').val("Failed to analyze data");
+                }
+              });
             } else {
-              return scope.data;
+              console.error("Failed to filter2 data", result3.error);
+              return $('#errorMessage').val("Failed to filter2 data");
             }
-          })();
-          tsData = transpose(tsData);
-          chartdata = new google.visualization.DataTable();
-          chartdata.addColumn("date", "Date");
-          chartdata.addColumn("number", "Raw");
-          chartdata.addColumn("number", "Filtered");
-          chartdata.addColumn("number", "Analysis");
-          chartdata.addRows(tsData);
-          chart.draw(chartdata, scope.options);
+          });
+        } else {
+          console.error("Failed to filter1 data", result2.error);
+          return $('#errorMessage').val("Failed to filter1 data");
         }
-      };
-      lscope.$watch('data', (function(newValue) {
-        return redraw(lscope, element);
-      }), true);
-      lscope.$watch('headers', (function(newValue) {
-        return redraw(lscope, element);
-      }), true);
-      lscope.$watch('options', (function(newValue) {
-        return redraw(lscope, element);
-      }), true);
-    },
-    template: '<div></div>'
+      });
+    } else {
+      console.error("Failed to load data", result1.error);
+      return $('#errorMessage').val("Failed to load data");
+    }
+  });
+};
+
+var timeseriesChart = function(lscope, element, attrs, controller) {
+  var chart, redraw;
+  chart = new google.visualization.ScatterChart(element[0]);
+  redraw = function(scope, element) {
+    var chartdata, headerWrapper, tsData;
+    if (scope.headers && scope.data && scope.data.length > 0) {
+      headerWrapper = [scope.headers];
+      tsData = (function() {
+        if (typeof scope.data === "string") {
+          try {
+            return JSON.parse(scope.data);
+          } catch (_error) {
+            return defaultSeries;
+          }
+        } else {
+          return scope.data;
+        }
+      })();
+      tsData = transpose(tsData);
+      chartdata = new google.visualization.DataTable();
+      chartdata.addColumn("date", "Date");
+      chartdata.addColumn("number", "Raw");
+      chartdata.addColumn("number", "Filtered");
+      chartdata.addColumn("number", "Analysis");
+      chartdata.addRows(tsData);
+      chart.draw(chartdata, scope.options);
+    }
   };
-});
+  lscope.$watch('data', (function(newValue) {
+    return redraw(lscope, element);
+  }), true);
+  lscope.$watch('headers', (function(newValue) {
+    return redraw(lscope, element);
+  }), true);
+  lscope.$watch('options', (function(newValue) {
+    return redraw(lscope, element);
+  }), true);
+  };
 
 transpose = function(arr) {
   return Object.keys(arr[0]).map(function(col) {

d493b9d2c069faa181b09fa215a3ed0553a3095d
Jon Peck <peckjon@gmail.com>
2017-02-08 11:41:37 -0800

    places classifier: jQuery conversion, standardize onready inits, move large demo images to s3 bucket, reconnect dropzone, refactor error-handling, autoscroll to results, clean up & document functions

--- JavaScript/places-demo/public/js/main.js
+++ JavaScript/places-demo/public/js/main.js
@@ -1,147 +1,99 @@
-window.Algorithmia = window.Algorithmia || {};
-Algorithmia.api_key = "simXy0OhJ0Yi7WR3m4osSRiRrdG1"
-var numTasks = 0;
+// init the Algorithmia client with your API key from https://algorithmia.com/user#credentials
+var algoClient = Algorithmia.client('simeyUbLXQ/R8Qga/3ZCRGcr2oR1');
 
-function callAlgorithm() {
-  startTask();
+var algorithms = {
+  classifier: 'deeplearning/Places365Classifier/0.1.9'
+};
 
-  // Get the img URL
-  var img = document.getElementById("imgUrl").value;
+/**
+ * once DOM is ready, update vars and add handlers
+ */
+$(document).ready(function() {
+  setInviteCode('places');
+  requireHttp($('#imgUrl'));
+  initDropzone();
+});
 
-  // Remove any whitespaces around the url
-  img = img.trim();
-
-  // Check if URL is an image
-    // Call the Image Resizing Function
-    getPlaces(img);
-
-  // getPlaces(img)
-  // document.getElementById("urlAddress").innerHTML = img;
-
-
-}
-
-function getPlaces(url) {
+/**
+ * call API on URL, get up to 7 results, and display them
+ * @param url
+ */
+var getPlaces = function(url) {
+  if(url) {
+    if(url.indexOf('http')==0) {$('#imgUrl').val(url);} //only display http URLs
+  } else {
+    url = $('#imgUrl').val();
+  }
+  if(url == "" || url=="http://") {
+    return hideWait('Please select an image, click the upload link, or enter a URL');
+  }
+  showWait();
   var input = {
     "image": url,
     "numResults": 7
   };
-  Algorithmia.client(Algorithmia.api_key)
-      .algo("algo://deeplearning/Places365Classifier/0.1.9")
-      .pipe(input)
-      .then(function (output) {
-        if (output.error) {
-          // Error Handling
-          var statusLabel = document.getElementById("status-label")
-          statusLabel.innerHTML = '<div class="alert alert-danger" role="alert">Uh Oh! Something went wrong: ' + output.error.message + ' </div>';
-          taskError();
-        } else {
-          addPlaces(output.result.predictions, url);
-          console.log("got output", output.result);
-        }
-      })
-}
+  $('#userImg').attr('src',url);
+  algoClient.algo(algorithms.classifier).pipe(input).then(function (output) {
+    if (output.error) {
+      endWait(output.error.message);
+    } else {
+      showPredictions(output.result.predictions);
+      endWait();
+    }
+  });
+};
 
-function addPlaces(result, img){
-
-  for (var i = 0; i < result.length; i++){
-    var num = result[i].prob * 100;
-    var n = num.toFixed(2);
-    n = n + "%"
-
-    var table = document.getElementById("tbody")
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-
-    cell1.innerHTML = '<span class="label label-success">'+result[i].class;+'</span>';
-    cell2.innerHTML = n;
+/**
+ * render tags and probabilities into
+ * @param result [{"class":string,"prob":number}]
+ */
+var showPredictions = function(result){
+  var html = '';
+  for (var i = 0; i < result.length; i++) {
+    var prob = (result[i].prob * 100).toFixed(2);
+    var tag = result[i].class.replace('_',' ');
+    html += '<tr><td><span class="label label-success">'+ tag+'</span></td><td>'+prob+'%</td></tr>';
   }
+  $('#results-tbody').html(html);
+  hideWait();
+};
 
-  // Check if img is a Data URI
-  var checkImg = img.split('://').shift();
-  var prefix = ['data'];
+/**
+ * show overlay, clear results
+ */
+var showWait = function() {
+  $('#overlay').removeClass('hidden');
+  $('#status-label').empty();
+  $("#results-tbody").empty();
+  $('#userImg').removeAttr('src');
+};
 
-  if (prefix.indexOf(checkImg) > -1){
-    // Retrieve original binary from Data API as base64
-    Algorithmia.client(Algorithmia.api_key)
-    .algo("algo://util/Data2Base64/0.1.0")
-    .pipe(img)
-    .then(function(output) {
-      if(output.error){
-        // Error Handling
-        var statusLabel = document.getElementById("status-label")
-        statusLabel.innerHTML = '<div class="alert alert-danger" role="alert">Uh Oh! Something went wrong: ' + output.error.message + ' </div>';
-        taskError();
-      } else {
-        // Decode base64 img
-        var outputImageOriginal = output.result;
-        var src = "data:image/jpeg;base64,";
-        src += outputImageOriginal;
-        // Add img to DOM
-        document.getElementById("userImg").src = src;
-        finishTask();
-      }
-    });
 
+/**
+ * close overlay and either reveal results or display errorMessage
+ * @param errorMessage
+ */
+var hideWait = function(errorMessage) {
+  $("#overlay").addClass("hidden");
+  if(errorMessage) {
+    $('#status-label').html('<div class="alert alert-danger" role="alert">' + errorMessage+ '</div>');
+    $('#results').addClass('hidden');
   } else {
-    // Add img to DOM
-    document.getElementById("userImg").src = img;
-    finishTask();
+    $('#results').removeClass('hidden');
+    $('html, body').animate({
+      scrollTop: $("#results").offset().top
+    }, 1000);
   }
+};
 
-
-
-}
-
-function analyzeDefault(img) {
-	document.getElementById("imgUrl").value = img;
-	callAlgorithm();
-}
-
-function startTask() {
-  numTasks++;
-  document.getElementById("overlay").classList.remove("hidden");
-
-  // Clear error messages
-  var statusLabel = document.getElementById("status-label")
-  statusLabel.innerHTML = "";
-  // Clear table
-  $("#tbody").empty();
-  // Clear image
-  var image = document.getElementById("userImg").src
-  image.src = "";
-}
-
-function finishTask() {
-  numTasks--;
-  if(numTasks <= 0) {
-    document.getElementById("overlay").classList.add("hidden");
-    document.getElementById("explainer").classList.add("hidden");
-    document.getElementById("results").classList.remove("hidden");
-    document.getElementById("social").classList.remove("invisible");
-    document.getElementById("marketing").classList.remove("hidden");
-  }
-}
-
-function taskError() {
-  numTasks = 0;
-  document.getElementById("overlay").classList.add("hidden");
-  document.getElementById("explainer").classList.add("display");
-  document.getElementById("explainer").classList.remove("hidden");
-  document.getElementById("results").classList.add("hidden");
-  document.getElementById("social").classList.add("invisible");
-  document.getElementById("marketing").classList.add("hidden");
-
-}
-
-
-function initDropzone() {
+/**
+ * initialize Dropzone component
+ */
+var initDropzone = function() {
   window.Dropzone.autoDiscover = false;
   var dropzone = new Dropzone("#file-dropzone", {
-    options: {
-      sending: function() {}
-    },
+    url: 'javascript:;',
+    options: {sending: function() {}},
     acceptedFiles: "image/*",
     previewTemplate: "<div></div>",
     maxFilesize: 10,
@@ -152,25 +104,17 @@ function initDropzone() {
   dropzone.__proto__.cancelUpload = function() {};
   dropzone.__proto__.uploadFile = function() {};
   dropzone.__proto__.uploadFiles = function() {};
-
   dropzone.on("processing", function(file) {
-    startTask();
-
+    showWait();
     var reader = new FileReader();
     reader.addEventListener("load", function () {
-      console.log("Calling algorithm with uploaded image.");
       getPlaces(reader.result);
       dropzone.removeFile(file);
     }, false);
     reader.readAsDataURL(file);
-    console.log("Reading uploaded image...");
   });
-
   dropzone.on("error", function(file, err) {
     dropzone.removeFile(file);
-    var statusLabel = document.getElementById("status-label")
-    statusLabel.innerHTML = '<div class="alert alert-danger" role="alert">Uh oh! ' + err + ' </div>';
-    taskError();
+    endWait(err);
   });
-}
-initDropzone();
+};

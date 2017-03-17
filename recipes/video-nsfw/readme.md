11420ed2adf158744e62555bb51d8d84a849d034
Jon Peck <peckjon@gmail.com>
2017-03-17 16:58:46 -0700

    Recipe: Video Nudity Detection

--- recipes/video-nsfw/readme.md
+++ recipes/video-nsfw/readme.md
@@ -1,6 +1,8 @@
-# Determine if a video contains nudity the VideoNudityDetection microservice
+# Remove NSFW sections from a video using the VideoNudityDetection microservice
 
-The [Video Nudity Detection microservice](https://algorithmia.com/algorithms/sfw/VideoNudityDetection) from Algorithmia is a straightforward API which takes in a video (by URL) and returns metadata, such as a list of object tags, for each frame of the video.
+The [Video Nudity Detection microservice](https://algorithmia.com/algorithms/sfw/VideoNudityDetection) from Algorithmia is an API which takes in a video (by URL) and detects which time-ranges may contain nudity.
+
+We can make use of this service, along with video-editing libraries, to strip out NSFW content from videos.
 
 For the full blog post related to this recipe, see http://blog.algorithmia.com [TBA].
 
@@ -12,22 +14,43 @@ Create a free [Algorithmia account](https://algorithmia.com/signup), and install
 pip install algorithmia
 ```
 
+Next, install the `moviepy` library, which will allow you to edit videos via Python:
+
+```
+pip install moviepy
+python -c "import imageio; imageio.plugins.ffmpeg.download()"
+```
+
+You might also need to hook up `ffmpeg`; simply the following Python script:
+
+```
+import imageio
+imageio.plugins.ffmpeg.download()
+```
+
 Detailed instructions can be found in the [blog post](http://blog.algorithmia.com/) [TBA].
 
 ## How To Run the Script
 
 First, edit the script and replace `your_api_key` with your [Algorithmia API Key](http://developers.algorithmia.com/basics/customizing-api-keys/)
 
-Also replace `http://example.com/somevideo.mpg` with the URL of a video you'd like to examine.
+Also replace `myfile.mp4` with the local path of a video you'd like to examine, and `output.mp4` with the filepath where output should be written.
 
 Use the command line, and navigate to the folder with your Python file and run:
 
 ```
-python video-metadata.py
+python video-nsfw.py
 ```
+
+## Notes
+
+You can adjust the value of `threshold` (max: 1) to tweak the nudity-detection sensitivity.
+
+If your system has trouble writing the output file, try replacing `libx264` with a different [codec](https://zulko.github.io/moviepy/ref/VideoClip/VideoClip.html#moviepy.video.VideoClip.VideoClip.write_videofile)
 
 ## Built With
 
 * [Algorithmia](https://algorithmia.com)
 * [Video Nudity Detection microservice](https://algorithmia.com/algorithms/sfw/VideoNudityDetection)
+* [moviepy](https://zulko.github.io/moviepy/install.html)
 
--- recipes/video-nsfw/video-nsfw.py
+++ recipes/video-nsfw/video-nsfw.py
@@ -1,13 +1,52 @@
 import Algorithmia
+import math
+from datetime import datetime
+from moviepy.editor import *
 
 
 def extract_metadata(source_uri):
-    """Determine if video contains nudity"""
+    """Determine what time-ranges of a video contain nudity"""
     algo = client.algo('sfw/VideoNudityDetection/0.1.0')
+    algo.set_options(timeout=15*60)
     data = {'source':source_uri}
     return algo.pipe(data).result
 
+
+def upload_file(local_file):
+    """Copy local_file to Algorithmia temporary datastore"""
+    video_dir = 'data://.my/videos/'
+    client.dir(video_dir).create()
+    video_file = video_dir+str(datetime.now().microsecond)
+    client.file(video_file).putFile(local_file)
+    return video_file
+
+
+def remove_nsfw(source_file, target_file, threshold=0.5, codec='libx264'):
+    """Strip out NSFW sections from source_file and write the cleaned video to target_file """
+    print "uploading %s" % source_file
+    data_uri = upload_file(source_file)
+    print "examining %s" % data_uri
+    nsfw_segments = extract_metadata(data_uri)
+    print "cleaning %s (threshold=%s)" % (source_file, threshold)
+    clip = VideoFileClip(source_file)
+    shift = 0
+    for segment in nsfw_segments['detections']:
+        if segment['average_confidence'] > threshold:
+            (start, stop) = (int(segment['start_time']), int(math.ceil(segment['stop_time'])))
+            print "removing %s-%s" % (start, stop)
+            clip = clip.cutout(start-shift,stop-shift)
+            shift += stop-start
+    print "writing %s (codec=%s)" % (target_file, codec)
+    clip.write_videofile(target_file, codec=codec)
+    client.file(data_uri).delete()
+
+
 # get your API key at algorithmia.com/user#credentials
 client = Algorithmia.client('your_api_key')
-video = "http://example.com/somevideo.mpg"
-print extract_metadata(video)
+input_video = 'myfile.mp4'
+output_video = 'output.mp4'
+threshold = 0.5
+codec = 'libx264'
+remove_nsfw(input_video, output_video, threshold, codec)
+
+# result = {'detections':[{'average_confidence':0.3236572316782259,'start_time':1.6229981521739132,'stop_time':2.9213966739130437},{'average_confidence':0.13879965838277722,'start_time':3.245996304347827,'stop_time':3.5705959347826086},{'average_confidence':0.1702812343894039,'start_time':6.167392978260869,'stop_time':6.8165922391304345},{'average_confidence':0.46762026507640286,'start_time':7.141191869565216,'stop_time':7.465791500000001},{'average_confidence':0.9650805749243591,'start_time':7.7903911304347835,'stop_time':9.413389282608698},{'average_confidence':0.5890913993091089,'start_time':9.737988913043477,'stop_time':10.062588543478265},{'average_confidence':0.7397092220286147,'start_time':10.387188173913044,'stop_time':10.711787804347827},{'average_confidence':1,'start_time':11.036387434782611,'stop_time':11.360987065217394},{'average_confidence':0.14343336627644027,'start_time':11.685586695652177,'stop_time':12.334785956521742},{'average_confidence':0.1844407251279336,'start_time':12.659385586956523,'stop_time':12.983985217391307},{'average_confidence':1,'start_time':13.308584847826092,'stop_time':13.633184478260869}]}

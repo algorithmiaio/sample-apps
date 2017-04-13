fe1108c7a9d15ed0b366d9389b581ea36d491b87
StephanieKim <stephanie.kim76@gmail.com>
2017-04-13 12:39:02 -0700

    Create color_extraction_recipe.py

---
+++ recipes/color-extraction/color_extraction_recipe.py
@@ -0,0 +1,47 @@
+"""Get image URL's from a site and extract unique color palette from images."""
+import Algorithmia
+import random
+
+client = Algorithmia.client("YOUR_API_KEY")
+
+
+def get_image(url):
+    """Retrieve images from site."""
+    algo = client.algo("diego/Getimagelinks/0.1.0")
+    if url.startswith("http:") or url.startswith("https:"):
+        try:
+            response = algo.pipe(url).result
+            print(response)
+            return response
+        except Algorithmia.algo_response.AlgoException as e:
+            print(e)
+    else:
+        raise Exception("Please pass in a valid url")
+
+
+def color_extractor(url):
+    """Extract top 15 colors from images."""
+    urls = get_image(url)
+    algo = client.algo("vagrant/ColorSchemeExtraction/0.2.0")
+    try:
+        response = [algo.pipe({"url": path}).result for path in urls]
+        print(response)
+        return response
+    except Algorithmia.algo_response.AlgoException as e:
+        print("Exception ")
+        print(e)
+
+
+def shuffle_colors(url):
+    """Create unique color paletted from images passed in."""
+    data = color_extractor(url)
+    a_list = []
+    for c in data:
+        for hex in c["colors"]:
+            a_list.append(hex["hex"])
+
+    new_color_scheme = random.sample(set(a_list), 5)
+    print(new_color_scheme)
+
+shuffle_colors(
+    "http://shop.nordstrom.com/c/designer-collections?origin=topnav&cm_sp=Top%20Navigation-_-Designer%20Collections")

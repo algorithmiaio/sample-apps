0d6d3841ecd698cefa61e87739fd86e444832f6f
Kenny Daniel <kenny@algorithmia.com>
2017-05-24 13:03:28 -0700

    Spark source cleanup

--- scala/twitter_deepfashion/src/main/scala/CollectTweets.scala
+++ scala/twitter_deepfashion/src/main/scala/CollectTweets.scala
@@ -31,111 +31,48 @@ object CollectTweets {
     println("Initializing Streaming Spark Context...")
 
     val SPARK_HOSTNAME = "local[*]"
-    // Can't use sparkSession with TwitterUtils.createStream
-//    val sparkSession = SparkSession
-//      .builder
-//      .master(SPARK_HOSTNAME)
-//      .appName("CollectTweets")
-//      .getOrCreate()
     val conf = new SparkConf().setAppName("CollectTweets").setMaster(SPARK_HOSTNAME)
     val sc = new SparkContext(conf)
     val ssc = new StreamingContext(sc, Seconds(1))
     ssc.checkpoint("/tmp/spark-test")
+    val sparky = SparkSession.builder.config(sc.getConf).getOrCreate()
 
     System.setProperty("twitter4j.oauth.consumerKey", auth.consumerKey)
     System.setProperty("twitter4j.oauth.consumerSecret", auth.consumerSecret)
     System.setProperty("twitter4j.oauth.accessToken", auth.accessToken)
     System.setProperty("twitter4j.oauth.accessTokenSecret", auth.accessTokenSecret)
 
-    // In order to ensure there are enough HTTP connections to the API server set NUM_CONNECTIONS appropriately
-    // for the amount of parallelism the spark task is doing.  This is primarily for local testing as in a cluster
-    // mode you can just increase the number of partitions and they will run on separate hosts
-
-    // Kenny in Scala Client throws an error too many arguments - might be why I'm getting timeout errors?
-    // Was comment in Patricks Scala demo code to put in a NUM_CONNECTIONS argument Algorithmia.client(ALGORITHMIA_API_KEY, NUM_CONNECTIONS
-    // val client = Algorithmia.client(auth.algorithmiaApiKey)
-
-    try {
-      println("Getting tweets")
-
-      val filters = Seq("streetstyle")
-      val tweets: DStream[Status] = TwitterUtils.createStream(ssc, None, filters)
-
-      val urls: DStream[String] = tweets.flatMap { tweet =>
+    val filters = Seq("streetstyle")
+    TwitterUtils.createStream(ssc, None, filters)
+      .flatMap { tweet =>
         println(s"Found tweet: $tweet")
         tweet.getMediaEntities.map(_.getMediaURL)
       }
-
-      val responses: DStream[Result] = urls.mapPartitions { partition =>
-        println("Processing tweets")
+      // Map tweet images to tags using Algorithmia
+      .mapPartitions { partition =>
         // Necessary to create client per-partition, since it may be distributed across cluster
         val client = Algorithmia.client(auth.algorithmiaApiKey)
         val algo = client.algo("algo://algorithmiahq/DeepFashion/0.1.1")
         partition.map { url =>
-          println(s"Processing tweet url: $url")
-//          algo.pipe(url).as[Result]
-          val json = algo.pipe(url).asJsonString
-          val result = Json.fromJson[Result](Json.parse(json)).get
-          println(s"Got tags for url $url $result")
-          result
+          algo.pipe(url).as[Result]
         }
       }
-
-      // urls.print()
-      // responses.print()
-
-      val sparky = SparkSession.builder.config(sc.getConf).getOrCreate()
-
-      val countsByUrl: DStream[(String, Int)] = responses
-        .flatMap(result => result.articles)
-        .map(art => (art.article_name, 1))
-        .reduceByKey(_ + _)
-        .updateStateByKey { (newCounts, x) =>
-          val sum = newCounts.sum
-          x.orElse(Some(0)).map(_ + sum)
-        }
-
-      countsByUrl.foreachRDD { (rdd: RDD[(String, Int)]) =>
-        val tagCounts = rdd.collect().toList
+      // Get tag articles
+      .flatMap(result => result.articles)
+      .map(art => (art.article_name, 1))
+      .reduceByKey(_ + _)
+      .updateStateByKey { (newCounts, x) =>
+        x.orElse(Some(0)).map(_ + newCounts.sum)
+      }
+      .foreachRDD { rdd =>
+        val tagCounts = rdd
+          .collect()
+          .toList
+          .sortBy(-_._2)
         if(tagCounts.nonEmpty) {
           println("Got tag counts: " + tagCounts)
         }
       }
-
-//          println(s"Aggregating fashion tags: $rdd")
-//
-//          import sparky.implicits._
-//
-//          try {
-//
-//            val articles = rdd.flatMap(_.articles) // Turn RDD[Result] -> Rdd[Articles]
-//            val article_names = articles.map(art => (art.article_name, 1)) // Turn that into Rdd[String] of just the article names
-//            val df = article_names.toDF("article")
-//            if (df.count() > 0) {
-//              df.show()
-//            }
-//
-//            df.createOrReplaceTempView("articles")
-//
-//            val imageCountsDataFrame = sparky.sql("select article, count(*) as total from articles group by article")
-//            if (imageCountsDataFrame.count() > 0) {
-//              imageCountsDataFrame.show()
-//            }
-//
-//          } catch {
-//            case e: Exception => {
-//              println("DF EXCEPTION", e)
-//            }
-//          }
-//        }
-//
-//      }
-
-    } catch {
-      case ex: Exception => {
-        println("createStream Exception")
-      }
-    }
 
     ssc.start()
     ssc.awaitTermination()

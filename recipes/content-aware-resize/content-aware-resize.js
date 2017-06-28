ef4670f4b32900490c7d25a2753bfad4d188a3f5
Jon Peck <peckjon@gmail.com>
2017-06-27 17:22:10 -0700

    Recipe: Content Aware Resize

--- recipes/content-aware-resize/content-aware-resize.js
+++ recipes/content-aware-resize/content-aware-resize.js
@@ -1,12 +1,17 @@
-var input = {
-   "height":300,
-   "width":150,
-   "binarize":true,
-   "image":"https://upload.wikimedia.org/wikipedia/commons/f/f7/Hickory_Golfer.jpg"
-};
-Algorithmia.client("your_api_key")
-  .algo("algo://media/ContentAwareResize/0.1.2")
-  .pipe(input)
-  .then(function(output) {
-     console.log(output);
+var algo = Algorithmia.client("your_api_key").algo("algo://media/ContentAwareResize/0.1.3");
+
+var resize = function(url, height, width, target) {
+  var input = {
+     "image":url,
+     "height":height,
+     "width":width,
+     "binarize":true
+  };
+  algo.pipe(input).then(function(response) {
+    document.getElementById(target).src = response.result;
   });
+};
+
+window.onload = function() {
+  resize("https://upload.wikimedia.org/wikipedia/commons/f/f7/Hickory_Golfer.jpg", 300, 150, "hero");
+};

1c35df66ef4ac9e10576408624f87698784d63e4
Jonathan Peck <peckjon@gmail.com>
2017-07-17 16:29:08 -0700

    style-thief recipe: add jQuery and correct input format

--- recipes/style-thief/style-thief.js
+++ recipes/style-thief/style-thief.js
@@ -1,14 +1,16 @@
 var algo = Algorithmia.client("your_api_key").algo("algo://bkyan/StyleThief/0.2.13");
-var geid = function(id) {return document.getElementById(id);}
 
 var run = function() {
   var input = {
-    "source": geid("source").value,
-    "style": geid("style").value,
-    "output": geid("output").value
-
+    "source": $("#source").val(),
+    "style": $("#style").val(),
+    "output": $("#output").val(),
+    "iterations" : 800
   };
+console.log(input);
+  $("#run").hide();
   algo.pipe(input).then(function(response) {
-    //TBD
+  $("#run").show();
+console.error(response);
   });
 };

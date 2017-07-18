f89ab3d4c5bbb5df668c6a351084eea19180640d
Jonathan Peck <peckjon@gmail.com>
2017-07-17 17:31:21 -0700

    style-thief recipe: bootstrap4, loading animation, and results formatting

--- recipes/style-thief/style-thief.js
+++ recipes/style-thief/style-thief.js
@@ -1,16 +1,23 @@
-var algo = Algorithmia.client("your_api_key").algo("algo://bkyan/StyleThief/0.2.13");
+var algo = Algorithmia.client("simABLGaZp3ZsXrLF9BhSGSnDGZ1").algo("algo://bkyan/StyleThief/0.2.13");
 
 var run = function() {
   var input = {
     "source": $("#source").val(),
     "style": $("#style").val(),
     "output": $("#output").val(),
-    "iterations" : 800
+    "iterations": 800,
+    "log_channel": "style-thief-demo"
   };
-console.log(input);
+  $("#message").text();
   $("#run").hide();
+  $("#loading").show();
   algo.pipe(input).then(function(response) {
-  $("#run").show();
-console.error(response);
+    $("#loading").hide();
+    $("#run").show();
+    if(response.error) {
+      $("#message").text(response.error.message);
+    } else {
+      $("#message").text(response.result.replaceAll('\n',' '))
+    }
   });
 };

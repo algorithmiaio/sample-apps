57b9c6fc364e65426008661867f9b1e58a798c8b
Jonathan Peck <peckjon@gmail.com>
2017-07-21 12:09:31 -0700

    style-thief recipe: fall back to non-S3 impl, use CatBase64 only

--- recipes/style-thief/style-thief.js
+++ recipes/style-thief/style-thief.js
@@ -1,17 +1,17 @@
-var client = Algorithmia.client("your_api_key");
+var client = Algorithmia.client("simc6QvB6kIC5nHmoA3hrsVerJ91"); //");
 var algo_stylethief = client.algo("algo://bkyan/StyleThief/0.2.13?timeout=3000");
 var algo_cat64 = client.algo("algo://util/CatBase64/0.1.1");
 
 var run = function() {
   var filename = new Date().getTime()+".jpg";
+  var outputURI = "data://.algo/bkyan/StyleThief/temp/"+filename;
   var input = {
     "source": $("#source").val(),
     "style": $("#style").val(),
     "output": filename,
-    "iterations": 100,
+    "iterations": 800,
     "log_channel": "style-thief-demo"
   };
-  var outputURI = "data://.algo/bkyan/StyleThief/temp/"+filename;
   $("#message").text("This will take 5-50 minutes, so please be patient!");
   $('#output').attr('src','');
   $("#run").hide();

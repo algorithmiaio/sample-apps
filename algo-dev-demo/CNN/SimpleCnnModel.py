222147e1155970bc48decee71743ba54d892f259
Justin Gage <gagejustins@MacBook-Pro-5.local>
2018-06-05 17:27:48 -0400

    source code for simpleCNN model

---
+++ algo-dev-demo/CNN/SimpleCnnModel.py
@@ -0,0 +1,44 @@
+import torch
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+class SimpleCNN(torch.nn.Module):
+    
+    #Our batch shape for input x is (3, 32, 32)
+    
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        
+        #Input channels = 3, output channels = 18
+        self.conv1 = torch.nn.Conv2d(3, 18, kernel_size=3, stride=1, padding=1)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        
+        #4608 input features, 64 output feature (see sizing flow below)
+        self.fc1 = torch.nn.Linear(18 * 16 * 16, 64)
+        
+        #64 input feature, 10 output features for our 10 defined classes
+        self.fc2 = torch.nn.Linear(64, 10)
+        
+    def forward(self, x):
+        
+        #Computes the activation of the first convolution
+        #Size changes from (3, 32, 32) to (18, 32, 32)
+        x = F.relu(self.conv1(x))
+        
+        #Pooling descales the input by a factor of the kernel_size (2)
+        #Size changes from (18, 32, 32) to (18, 16, 16)
+        x = self.pool(x)
+        
+        #Reshape data to input to the input layer of the neural net
+        #Size changes from (18, 16, 16) to (1, 4608)
+        #Recall that the -1 infers this dimension from the other given dimension
+        x = x.view(-1, 18 * 16 *16)
+        
+        #Computes the activation of the first fully connected layer
+        #Size changes from (1, 4608) to (1, 64)
+        x = F.relu(self.fc1(x))
+        
+        #Computes the second fully connected layer (activation applied later)
+        #Size changes from (1, 64) to (1, 10)
+        x = self.fc2(x)
+        return(x)

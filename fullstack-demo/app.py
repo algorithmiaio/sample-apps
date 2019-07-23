b8a3990a383a1e2633b536328f1e7ef783b8ec43
Jonathan Peck <peckjon@gmail.com>
2019-07-23 16:13:57 -0700

    DS-24 shebang

--- fullstack-demo/app.py
+++ fullstack-demo/app.py
@@ -1,9 +1,72 @@
-from flask import Flask
-from flask import render_template
+import flask
+import flask_login
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+app.secret_key = 'CHANGE_ME!'
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+users = {'foo@bar.tld': {'password': 'secret'}}
 
 @app.route('/')
 def home():
     message='Hello World!!'
-    return render_template('index.htm',message=message)
+    return flask.render_template('index.htm',message=message)
+
+
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+    user = User()
+    user.id = email
+    user.is_authenticated = request.form['password'] == users[email]['password']
+    return user
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+    email = flask.request.form['email']
+    if email in users and flask.request.form['password'] == users[email]['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+
+    return 'Bad login'
+
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized'
+
+
+if __name__ == '__main__':
+   app.run()

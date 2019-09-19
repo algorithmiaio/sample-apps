e465361dff3f015959edb02f462f872dc7135e1c
Nathan Lyle Black <nblack@algorithmia.io>
2019-09-19 14:57:38 -0700

    FRONTEND-1737 Remove session token value from aws creds

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,6 @@ module.exports = function(grunt) {
       options: {
         accessKeyId: '<%= aws.key %>',
         secretAccessKey: '<%= aws.secret %>',
-        sessionToken: '<%= aws.session %>',
         bucket: 'demos.algorithmia.com',
         region: 'us-east-1',
         uploadConcurrency: 5,
